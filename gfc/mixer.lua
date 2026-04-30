161c4ecfa337734acb5b7affa4d215c9a6cf620e
voroxity <mechlabgh@gmail.com>
2026-04-29 22:57:58 -0400

    added computeTranslationForces() and mixer() functions
    added computeTranslationForces() and mixer() functions.
    TODO: add support for non prop blocks to computeRotationForces() function as it will break if a point is (0,0,0) or if the position vector is parallel to the facing axis.

--- gfc/mixer.lua
+++ gfc/mixer.lua
@@ -9,7 +9,7 @@ local function round(x)-- rounds to the nearest intager
 end
 
 local function pressure(v)-- returns the presure at a point (v) on the sublevel.
-    local o = sublevel.getLastPos().position-- gets the position of the computer relitive to the world, not the sublevel.
+    local o = sublevel.getLogicalPose().position-- gets the position of the computer relitive to the world, not the sublevel.
     return aero.getAirPressure(v + o)-- returns the air pressure at (v + o).
 end
 
@@ -26,23 +26,27 @@ local function prop_RPM(f,c,p)-- gets the rpm for prop that meets the required f
     -- p: air pressure at blocks center.
     return round(f / (c * p))
 end
+
 --[[
--- profile is a table the stores specific craft data for the mixer and other functions.
--- i: the index number. think of this a a blocks id.
+-- TODO: as of 29/4/2026 only propellers are suported. add supported for lift blocks
+-- profile is a table the stores specific craft data for the mixer and other functions
+-- i: the index number. think of this a a blocks id
 -- pos: position vector of the block relitive to center of mass.
 --      this represents the moment arm for force calculations.
 -- facing: a unit vector that denotes the direction the block is facing.
 -- type: the type of block.
---  "B" = propeller baring.
---  "W" = wodden propeller.
---  "A" = andesite propller.
+--  "B" = propeller baring 
+--  "W" = wodden propeller
+--  "A" = andesite propller
 -- sails: the number of sailes if the block is a propeller baring.
-profile = { [i] = {pos = vector.new(x, y, z), facing = vector.new(x, y, z), type = "A", sails = 0} }
+-- constant: block related config constants
+profile = { [i] = {pos = vector.new(x, y, z), facing = vector.new(x, y, z), type = "A", sails = #, constant = #} }
 ]]
-local function computeForces(profile, T)
+-- compute the forces needed to achieve input torque vector
+local function computeRotationForces(profile, T)
     local data  = { {}, {}, {} }
     for i = 1, #profile do
-        local P = profile[i]
+        local p = profile[i]
         local ai = p.facing
         local ci = p:cross(ai)
         data[1][i] = ci.x
@@ -58,17 +62,67 @@ local function computeForces(profile, T)
         print("[moment_solver] " .. warning)
     end
     local f_mat = Ct * y
-    local forces = {}
+    local Rforces = {}
     for i = 1, #profile do
-        forces[i] = f_mat[i][1]
+        Rforces[i] = f_mat[i][1]
     end
     --verification
-    local T_mat = C * f_mat                  -- (3 x 1)
+    local T_mat = C * f_mat-- (3 x 1)
     local T_achieved = vector.new(T_mat[1][1], T_mat[2][1], T_mat[3][1])
 
     local diff = T_achieved - T
     local err  = math.sqrt(diff.x^2 + diff.y^2 + diff.z^2)
-    return forces, T_achieved, err
+    return Rforces, T_achieved, err
 end
 
-return computeForces
+local function computeTranslationForces(profile,F)
+    local x = 0-- x axis
+    local y = 0-- y axis
+    local z = 0-- z axis
+    for i=1, #profile do
+        local p = profile[i]
+        local facing = p.facing
+        if p.type == "A" or "B" or "W" then-- checks to see if entry is a prop block
+            x = x + math.abs(facing.x)
+            y = y + math.abs(facing.y)
+            z = z + math.abs(facing.z)
+        end
+    end
+    local X = F.x / x
+    local Y = F.y / y
+    local Z = F.z / z
+    local F_out vector.new(X, Y, Z)
+    local TForces = {}
+    for i = 1, #profile do
+        local p = profile[i]
+        if p.type == "A" or "B" or "W" then-- checks to see if entry is a prop block
+            TForces[i] = F_out:dot(p.facing)
+        else
+            TForces[i] = 0
+        end
+    end
+    return TForces
+end
+
+local function mixer(profile, T, F)
+    local Rforces, T_achieved, err = computeRotationForces(profile, T)
+    local TForces = computeTranslationForces(profile,F)
+    local RPM = {}
+    for i = 1, #profile do
+        local P = profile[i]
+        local f = Rforces[i] + TForces[i]
+        local s = P.sails
+        local c = P.constant
+        local p = pressure(P.pos)
+        if P.type == "A" or "W" then-- checks to see if entry is a prop block
+            RPM[i] = prop_RPM(f,c,p)
+        elseif P.type == "B" then
+            RPM[i] = bearing_RPM(f,s,c,p)
+        else
+            RPM[i] = 0
+        end
+    end
+    return RPM, T_achieved, err
+end
+
+return mixer
