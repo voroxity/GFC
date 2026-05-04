58d420e6d9ce2710a5021a54b8f8a3cd709b813f
voroxity <mechlabgh@gmail.com>
2026-05-03 22:47:45 -0400

    Update main.lua

--- gfc/main.lua
+++ gfc/main.lua
@@ -1,4 +1,5 @@
 local so3 = require("SO3")
+local mixer = require("mixer")
 
 local tw = require("tw")
 tw.init("right")
@@ -9,9 +10,9 @@ m.clear()
 term.redirect(m)
 
 while true do
-    local data = tw.update()
+    local data = tw.update()--pull data from typewritter
 
-    local input = {
+    local input = {--defaults for pilot inputs
         q_desired = quaternion.new(vector.new(0, 1, 0), 0),
         v_desired = vector.new(0, 0, 0)
     }
@@ -21,7 +22,7 @@ while true do
         input.v_desired = vector.new(data.throttle, data.lift, data.strafe)
         -- print(string.format("Throttle: %d, strafe: %d, lift: %d, turn: %d", data.throttle, data.strafe, data.lift, data.turn));
     end
-    
+
     local q = sublevel.getLastPose().orientation
     local I = sublevel.getInertiaTensor()
     local v_world = sublevel.getVelocity()
@@ -29,7 +30,12 @@ while true do
     local mass = sublevel.getMass()
     local g = aero.getDefault().gravity
 
-    local result = so3(input.q_desired, input.v_desired, I, q, omega, mass, g, v_world)
-    print(result)
+    local force, torque = so3(input.q_desired, input.v_desired, I, q, omega, mass, g, v_world)
+    print(string.format("force vector: %, torque vector: %", force, torque))
+
+    local RPM, T_achieved, err = mixer(profile, torque, force)
+    -- RPM: a table with the same length as profile. each entry should be an intager that is positive or negitive.
+    -- T_achieved: rotation math output. should be the same as torque.
+    -- err: a value that represents the error of the mixer rotation math.
     os.sleep(0.5)
 end
